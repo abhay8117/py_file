c9c5133dec18bd75a0bf4c10dc4790985f82adcf
Abhay Kumar <79265467+abhay8117@users.noreply.github.com>
2023-07-24 20:23:49 +0530

    string operation

---
+++ Assignment on string.py
@@ -0,0 +1,27 @@
+
+x=[1,2,3,4,5,10,20,19]
+y=[1,2,3,4,5]
+z=['abc','xyz']
+print(type(x))
+print(type(z))
+
+print(10 in x)
+
+for i in enumerate(x):
+    print(i)
+
+for idx,i in enumerate(x):
+    if i==20:
+        print(idx,i)
+        
+x1=[5,9,30,14,4.5,10,20,19]
+max_no=
+    count=0
+    
+
+
+
+
+
+
+
